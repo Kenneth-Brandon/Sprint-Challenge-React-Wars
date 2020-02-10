dce113e2a7a63591cb7bc622ec3138e3f40ae268
unknown <ken.brandon@aol.com>
2020-02-09 20:06:17 -0700

    app.js built out

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+import styled from "styled-components";
+import "./App.css";
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+import Character from "./components/Character";
+import PaginationButton from "./components/PaginationButton";
+import Search from "./components/Search";
+
+const App = props => {
+  const [characters, setCharacters] = useState([]);
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previousPageUrl, setPreviousPageUrl] = useState();
+  const [search, setSearch] = useState("");
+
+  function getCharacters(characterUrl) {
+    axios.get(characterUrl).then(response => {
+      console.log(response.data);
+      setNextPageUrl(response.data.next);
+      setPreviousPageUrl(response.data.previous);
+      setCharacters(response.data.results);
+      console.log(
+        "Pagination Urls: ",
+        response.data.next,
+        response.data.previous
+      );
+    });
+  }
+
+  const initialCharactersUrl = `https://swapi.co/api/people`;
+
+  useEffect(() => {
+    getCharacters(initialCharactersUrl);
+  }, []);
+
+  if (!characters) return <h1>Loading</h1>;
+
+  const filteredCharacters = characters.filter(character => {
+    return character.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
 
   return (
-    <div className="App">
+    <StyledApp className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <StyledButtonContainer>
+        <PaginationButton
+          text="Previous"
+          url={previousPageUrl}
+          callback={() => getCharacters(previousPageUrl)}
+        />
+        <PaginationButton
+          text="Next"
+          url={nextPageUrl}
+          callback={() => {
+            getCharacters(nextPageUrl);
+          }}
+        />
+        <Search search={search} setSearch={setSearch} />
+      </StyledButtonContainer>
+      <StyledCharacterContainer>
+        {filteredCharacters.map(character => {
+          <Character key={character.url} characterData={character} />;
+        })}
+      </StyledCharacterContainer>
+    </StyledApp>
   );
-}
+};
 
 export default App;
